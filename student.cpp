7207159db649cbdbba9365bd570ee5b4279403d0
anjila04 <117088502+anjila04@users.noreply.github.com>
2024-05-09 11:04:20 -0400

    Create student.cpp

---
+++ student.cpp
@@ -0,0 +1,15 @@
+// Name:Anjila Raya
+// Email: anjila.raya48@myhunter.cuny.edu
+// Prints Hello! (C++).
+
+#include <iostream>
+
+int main() {
+    // Print "Hello, World!" to the screen
+    std::cout << "Hello, World!" << std::endl;
+    
+    // Print "Hello, C++!" to the screen
+    std::cout << "Hello, C++!" << std::endl;
+
+    return 0;
+}
